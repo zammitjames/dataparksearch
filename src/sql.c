66418f9257a14df7664533cdec16514b622de812
dp.maxime <dp.maxime@2656b337-cf4f-0410-a0fa-a59f4e0cf5dc>
2008-10-21 21:35:00 +0000

    Update to the latest snapshot released as of October 22

--- src/sql.c
+++ src/sql.c
@@ -3491,10 +3491,12 @@ int DpsClearDBSQL(DPS_AGENT *Indexer, DPS_DB *db) {
 					    DpsSQLFree(&SQLres);
 					    return(DPS_ERROR);
 					  }
-					  if(DPS_OK!=DpsDeleteURL(Indexer,&Doc,db)) {
+					  if (db->DBMode == DPS_DBMODE_CACHE)
+					    if (DPS_OK != (rc = DpsDeleteWordFromURL(Indexer, &Doc, db))) return rc;
+/*					  if(DPS_OK!=DpsDeleteURL(Indexer,&Doc,db)) {
 					    DpsSQLFree(&SQLres);
 					    return(DPS_ERROR);
-					  }
+					  }*/
 					}
 					if (rows_num < url_num) break;
 					continue;
