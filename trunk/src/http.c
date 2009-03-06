1116a16f03e67619d24a6475208e0dd035ec951b
dp.maxime <dp.maxime@2656b337-cf4f-0410-a0fa-a59f4e0cf5dc>
2009-03-06 13:46:58 +0000

    Fix for possible trap on an empty document

--- trunk/src/http.c
+++ trunk/src/http.c
@@ -132,16 +132,19 @@ void DpsParseHTTPResponse(DPS_AGENT *Indexer, DPS_DOCUMENT *Doc) {
 
 	/* Cut HTTP response header first        */
 	for(token=Doc->Buf.buf;*token;token++){
-		if(!strncmp(token,"\r\n\r\n",4)){
+	  if(!strncmp(token,"\r\n\r\n",4)){
+	    if (token < Doc->Buf.buf + Doc->Buf.size - 4) {
 			*token='\0';
 			Doc->Buf.content = token + 4;
-			break;
-		}else
-		if(!strncmp(token,"\n\n",2)){
+	    }
+	    break;
+	  } else if(!strncmp(token,"\n\n",2)){
+	    if (token < Doc->Buf.buf + Doc->Buf.size - 2) {
 			*token='\0';
 			Doc->Buf.content = token + 2;
-			break;
-		}
+	    }
+	    break;
+	  }
 	}
 	
 	/* Bad response, return */
@@ -149,8 +152,6 @@ void DpsParseHTTPResponse(DPS_AGENT *Indexer, DPS_DOCUMENT *Doc) {
 	  if (token < Doc->Buf.buf + Doc->Buf.size - 4) {
 	    if (token[2] == CR_CHAR) Doc->Buf.content = token + 4;
 	    else Doc->Buf.content = token + 2;
-	  } else {
-	    return;
 	  }
 	}
 	
@@ -194,5 +195,9 @@ void DpsParseHTTPResponse(DPS_AGENT *Indexer, DPS_DOCUMENT *Doc) {
 	DpsDSTRFree(&header);
 	DPS_FREE(headers);
 	
+	/* Bad response, return */
+	if(!Doc->Buf.content) {
+	    return;
+	}
 	DpsVarListInsInt(&Doc->Sections,"Content-Length",Doc->Buf.buf-Doc->Buf.content+(int)Doc->Buf.size);
 }
