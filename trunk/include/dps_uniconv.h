6f5100e2ad2c46ade984b641c999419892358dd3
dp.maxime <dp.maxime@2656b337-cf4f-0410-a0fa-a59f4e0cf5dc>
2011-03-13 02:21:52 +0000

    Charset and language guesser modification.

--- trunk/include/dps_uniconv.h
+++ trunk/include/dps_uniconv.h
@@ -198,7 +198,7 @@ typedef struct dps_conv_st {
 #define DPS_LM_MAXGRAM1		3
 #define DPS_LM_MAXGRAM2		(3 * sizeof(dpsunicode_t))
 #define DPS_LM_HASHMASK		0x07FF
-#define DPS_LM_TOPCNT           50 /*150*/
+#define DPS_LM_TOPCNT           150
 
 typedef struct {
 	size_t		count, index;
