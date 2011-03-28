edf0fed19b1b6d3fb3b9ace26d065566f6166b8e
dp.maxime <dp.maxime@2656b337-cf4f-0410-a0fa-a59f4e0cf5dc>
2011-03-28 11:42:47 +0000

    Fixes for API Sanity Autotest.

--- trunk/include/dps_acronym.h
+++ trunk/include/dps_acronym.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2005 Datapark corp. All rights reserved.
+/* Copyright (C) 2005-2011 DataPark Ltd. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -19,6 +19,7 @@
 #define _DPS_ACRONYM_H
 
 #include "dps_config.h"
+#include "dps_common.h"
 
 
 
