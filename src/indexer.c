156592d7f290e0f377e03a00d83193c7f97a2b46
dp.maxime <dp.maxime@2656b337-cf4f-0410-a0fa-a59f4e0cf5dc>
2012-08-19 04:59:17 +0000

    fix crossword comparison

--- src/indexer.c
+++ src/indexer.c
@@ -2742,7 +2742,7 @@ __C_LINK int __DPSCALL DpsIndexNextURL(DPS_AGENT *Indexer){
 			      DpsStopListFind(&Indexer->Conf->StopWords, p_word->uword, (Indexer->flags & DPS_FLAG_STOPWORDS_LOOSE) ? lang : "" ) != NULL);
 		    if (rc) p_word->weight = 0;
 		    for(wordnum = 1; wordnum < Doc->CrossWords.ncrosswords; wordnum++) {
-		      if (DpsWordCmp(p_word, Doc->CrossWords.CrossWord + wordnum) == 0) {
+		      if (DpsCrossCmp(p_word, Doc->CrossWords.CrossWord + wordnum) == 0) {
 			if (rc) Doc->CrossWords.CrossWord[wordnum].weight = 0;
 		      } else {
 			p_word = Doc->CrossWords.CrossWord + wordnum;
