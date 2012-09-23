e975a225a31620ce1763ac684a422a9b850ef69c
dp.maxime <dp.maxime@2656b337-cf4f-0410-a0fa-a59f4e0cf5dc>
2012-09-23 03:22:08 +0000

    Put sentences into the SEA in the order they appear in the document

--- include/dps_sea.h
+++ include/dps_sea.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2005 Datapark corp. All rights reserved.
+/* Copyright (C) 2005-2012 Datapark corp. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -28,6 +28,7 @@ typedef struct {
   double       Oi;
   double       di;
   size_t       len;
+  size_t       order; /* original order of the sentence in the document */
 } DPS_SENTENCE;
 
 typedef struct {
